e36a8b69fa030eb9414b55cfc8c3b69efc95ecb8
Luis <Luis@DESKTOP-MK4RUFD>
2021-11-29 20:04:52 -0500

    El primer proyecto que hicimos en clase con GitHub

---
+++ src/main/java/mi/primer/proyecto/lo/Animal.java
@@ -0,0 +1,11 @@
+package mi.primer.proyecto.lo;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("mi primer proyecto con Github");
+
+	}
+
+}
