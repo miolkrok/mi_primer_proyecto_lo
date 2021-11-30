d76ba24e8e7d47a7db20855ed16ab95c7aecdcb6
Luis <Luis@DESKTOP-MK4RUFD>
2021-11-29 20:16:04 -0500

    otro comentario mas

--- src/main/java/mi/primer/proyecto/lo/Animal.java
+++ src/main/java/mi/primer/proyecto/lo/Animal.java
@@ -5,6 +5,8 @@ public class Animal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("mi primer proyecto con Github");
+		System.out.println("mi segundo proyecto con Github");
+		System.out.println("mi tercer proyecto con Github");
 
 	}
 
